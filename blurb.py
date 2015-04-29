d84c79cd0345dd739d50ba5f09b3768d969cf78d
Konstantin Itskov <konstantin.itskov@kovits.com>
2015-04-29 10:45:12 -0400

    Lexicon file that contains a number of word pronunciation corrections.

--- blurb.py
+++ blurb.py
@@ -4,15 +4,16 @@
 Dependencies: padsp, swift, xsel
 """
 
+import subprocess
+import re
+import sys
+
 __version__ = 1.0
 __date__ = '2014-10-26'
 __author__ = 'Konstantin Itskov <konstantin.itskov@kovits.com>'
 
-import subprocess
-import re
-
 rate = 1.1
-x_weak = r'[,]'
+x_weak = r'[,"]'
 weak = r'[.:;!•—]'
 skip = r'[_-]'
 
@@ -30,16 +31,27 @@ def main():
     else:
         text = subprocess.check_output(["xsel"])
         text = text.decode('utf-8')
+
+        # Special Cases
+        ptn = re.compile(r'[’]')
+        text = re.sub(ptn, '\'', text)
+        ptn = re.compile(r'[“”]')
+        text = re.sub(ptn, '"', text)
+
+        # Intonation and Pauses
         ptn = re.compile(x_weak)
         text = re.sub(ptn, '<break strength="x-weak"/>', text)
         ptn = re.compile(weak)
         text = re.sub(ptn, '<break strength="weak"/>', text)
         ptn = re.compile(skip)
         text = re.sub(ptn, ' ', text)
+
+        # Globals
         text = '<prosody rate="' + str(rate) + '">' + text + '</prosody>'
-        print(text)
-        cmd = "padsp swift -e \"utf-8\" " + text
+        path = sys.path[0] + "/lexicon.txt"
+        cmd = "padsp swift -l " + path + " -e \"utf-8\" " + text
         subprocess.Popen(cmd.split())
+
 
 if __name__ == '__main__':
     main()
